295ad759005189071e64561f590fee287925fbb4
Patrick777 <ekireev@inbox.ru>
2023-05-25 22:40:06 +0300

    add division dy words count

--- models.py
+++ models.py
@@ -1,14 +1,9 @@
-import logging
-import math
-import textwrap
-import numpy as np
-import ujson
 import google.protobuf.text_format as pbtext
-from protobuf_to_dict import protobuf_to_dict, dict_to_protobuf
 import json
 import itertools as it
 import re
 from pprint import pprint
+from protobuf_to_dict import protobuf_to_dict, dict_to_protobuf
 
 
 def get_words(input_data: str):
@@ -24,7 +19,7 @@ def chunked(sp, n):
     return [sp[i:i + n] for i in range(0, len(sp), n)]
 
 
-def punctuation_break(input_data: str):
+def punctuation_break(input_data):
     datas = next(get_words(input_data))
     out = []
     start = 0
@@ -43,26 +38,34 @@ def phrase_time(input_data: str, num_seconds_max, gap_between_sec):
     out = []
     length = 0
     start = 0
+    gap = 0
     for i in range(len(datas)):
         start_time, end_time = datas[i].get('start'), datas[i].get('end')
-        # print(length, datas[i])
-        if length <= num_seconds_max:
-            length += end_time - start_time
-        if length > num_seconds_max:
-            temp = datas[start: i + 1]
+
+        def duration_count(text):
+            words_count = 7
+            end = start + min([words_count, (i + 1) - start])
+            temp = text[start: end]
+            for key, value in enumerate(temp):
+                value['end'] = round((value['end'] + gap), 2)
+                value['start'] = round((value['start'] + gap), 2)
             out.append(temp)
-            print([d.get('word') for d in temp])
-            start = i + 1
+            return end
+
+        if length < num_seconds_max:
+            length += end_time - start_time
+        if length >= num_seconds_max:
+            gap += gap_between_sec
+            start = duration_count(datas)
             length = 0
         if i == len(datas) - 1:
-            temp = datas[start: i + 1]
-            # _temp = [d.get('word') for d in temp]
-            out.append(temp)
-            # print(temp)
-    # pprint(out)
-    return out
+            diff = list(it.filterfalse(list(it.chain.from_iterable(out)).__contains__, iter(datas)))
+            for j in range(len(diff)):
+                start = duration_count(datas)
+    pprint(list(filter(None, out)))
+    return list(filter(None, out))
 
 
 # pprint(get_words('test.json').__next__())
 # pprint(punctuation_break('test.json').__next__())
-phrase_time('test_1.json', 3, 0.2)
+phrase_time('test_1.json', 1, 5)
