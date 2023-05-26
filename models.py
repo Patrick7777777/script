c80a55328fcb5dddccc952255cecc9d66fa488dd
Patrick777 <ekireev@inbox.ru>
2023-05-26 12:31:43 +0300

    some improvements

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import google.protobuf.text_format as pbtext
 import json
+import ujson
 import itertools as it
 import re
 from pprint import pprint
@@ -33,7 +34,7 @@ def punctuation_break(input_data):
     yield out
 
 
-def phrase_time(input_data: str, num_seconds_max, gap_between_sec):
+def phrase_time(input_data: str, num_seconds_max, gap_between_sec, number_of_words_in_phrase):
     datas = next(get_words(input_data))
     out = []
     length = 0
@@ -42,30 +43,38 @@ def phrase_time(input_data: str, num_seconds_max, gap_between_sec):
     for i in range(len(datas)):
         start_time, end_time = datas[i].get('start'), datas[i].get('end')
 
-        def duration_count(text):
-            words_count = 7
+        def chunk_division(text, words_count):
             end = start + min([words_count, (i + 1) - start])
             temp = text[start: end]
+            phrase = []
             for key, value in enumerate(temp):
                 value['end'] = round((value['end'] + gap), 2)
                 value['start'] = round((value['start'] + gap), 2)
-            out.append(temp)
-            return end
-
+                phrase.append(value.get('word'))
+            # if bool(temp):
+            #     out.append({"result": temp, "text": ' '.join(phrase)})
+            out.append({"result": temp, "text": ' '.join(phrase)})
+            yield end
         if length < num_seconds_max:
             length += end_time - start_time
         if length >= num_seconds_max:
             gap += gap_between_sec
-            start = duration_count(datas)
+            start = next(chunk_division(datas, number_of_words_in_phrase))
             length = 0
         if i == len(datas) - 1:
             diff = list(it.filterfalse(list(it.chain.from_iterable(out)).__contains__, iter(datas)))
             for j in range(len(diff)):
-                start = duration_count(datas)
-    pprint(list(filter(None, out)))
-    return list(filter(None, out))
+                if start != len(diff):
+                    start = next(chunk_division(diff, number_of_words_in_phrase))
+                else:
+                    break
+
+    pprint(out)
+    # json_string = json.dumps(out)
+    # print(json_string)
+    return out
 
 
 # pprint(get_words('test.json').__next__())
 # pprint(punctuation_break('test.json').__next__())
-phrase_time('test_1.json', 1, 5)
+phrase_time('test.json', 0.1, 1, 100)
