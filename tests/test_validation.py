39a20da53f27854a8876982e4ebf2cfea1cf4114
Patrick777 <ekireev@inbox.ru>
2023-06-11 18:12:25 +0300

    improved first test

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -4,16 +4,23 @@ import json
 from functions import validation, normalize
 
 
+# test for one word in line
 @pytest.mark.parametrize(
     'num_chars, time, num_words, gap', [
+        # letters
+        (2, 20, 20, 0),
+        (1, 20, 20, 0),
+        (0, 20, 20, 0),
+        # time
         (40, 2, 40, 0),
         (40, 1, 40, 0),
-        (40, 0, 40, 0)
+        (40, 0, 40, 0),
+        # words
+        (40, 40, 1, 0)
     ])
-def test_validation(num_chars, time, num_words, gap, words):
+def test_validation_one_word(num_chars, time, num_words, gap, words):
     result = validation(next(normalize(words, num_chars, time, num_words)), gap)
     assert result == json.load(open('tests/src/test_1_expected.json', 'r'))
-
 
 
 
