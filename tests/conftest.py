16836e6fdb77eebec1a46faa0dc1b1261f7817a0
Patrick777 <ekireev@inbox.ru>
2023-06-12 12:50:39 +0300

    made three tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,18 @@
 import pytest
 
-from functions import get_words
+from functions import get_words, chunked
 
 
 @pytest.fixture(params=['./tests/src/test_in.json'])
 def words(request):
     return next(get_words(request.param))
 
+
+# def make_expected(path):
+#     wrds = next(get_words(path))
+#     chnks = next(chunked(wrds, [1, 3]))
+#     print(chnks)
+#
+#
+# make_expected('./tests/src/test_in.json')
 
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -23,4 +23,61 @@ def test_validation_one_word(num_chars, time, num_words, gap, words):
     assert result == json.load(open('tests/src/test_1_expected.json', 'r'))
 
 
+# test for two words in line
+@pytest.mark.parametrize(
+    'num_chars, time, num_words, gap', [
+        # letters
+        (6, 40, 40, 0),
+        # time
+        (40, 4, 40, 0),
+        # words
+        (40, 40, 2, 0)
+    ])
+def test_validation_two_words(num_chars, time, num_words, gap, words):
+    result = validation(next(normalize(words, num_chars, time, num_words)), gap)
+    assert result == json.load(open('tests/src/test_2_expected.json', 'r'))
 
+
+# test for three words in line
+@pytest.mark.parametrize(
+    'num_chars, time, num_words, gap', [
+        # letters
+        (9, 40, 40, 0),
+        # time
+        (40, 6, 40, 0),
+        # words
+        (40, 40, 3, 0)
+    ])
+def test_validation_three_words(num_chars, time, num_words, gap, words):
+    result = validation(next(normalize(words, num_chars, time, num_words)), gap)
+    assert result == json.load(open('tests/src/test_3_expected.json', 'r'))
+
+
+# test for four words in line
+@pytest.mark.parametrize(
+    'num_chars, time, num_words, gap', [
+        # letters
+        (12, 40, 40, 0),
+        # time
+        (40, 8, 40, 0),
+        # words
+        (40, 40, 4, 0)
+    ])
+def test_validation_four_words(num_chars, time, num_words, gap, words):
+    result = validation(next(normalize(words, num_chars, time, num_words)), gap)
+    assert result == json.load(open('tests/src/test_4_expected.json', 'r'))
+
+
+# test for validation one word and gap 0.5s
+@pytest.mark.parametrize(
+    'num_chars, time, num_words, gap', [
+        # letters
+        (2, 40, 40, 0.5),
+        # time
+        (40, 2, 40, 0.5),
+        # words
+        (40, 40, 1, 0.5)
+    ])
+def test_validation_one_word_gap_05(num_chars, time, num_words, gap, words):
+    result = validation(next(normalize(words, num_chars, time, num_words)), gap)
+    assert result == json.load(open('tests/src/test_1_gap_05_expected.json', 'r'))
